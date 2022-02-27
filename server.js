c8bba8a3614bfad8fd6f7e50be4bf7b7176d82f9
K3A101 <keisha.alexander@hva.nl>
2022-02-27 15:30:00 +0100

    Routes gemaakt in de server

--- server.js
+++ server.js
@@ -1,11 +1,35 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const port = 3000;
+
+
+
 
 app.get('/', (req, res) => {
     res.send('Hi World!')
-})
+});
+
+app.get('/home', (req, res) => {
+    res.send('Voor de Homepagina')
+});
+
+app.get('/profiel', (req, res) => {
+    res.send('Voor de profielpagina')
+});
+
+app.get('/match', (req, res) => {
+    res.send('Voor de match detailpagina')
+});
+
+
+// Error handling
+app.use((req, res, next) => {
+    res.status(404).send('Error 404: File not found')
+});
+
+//serve static files
+app.use(express.static('public'))
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
-})
+});
