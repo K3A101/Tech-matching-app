ff650d68cb821f4fe73f4fdba79e0c80cd0c0434
K3A101 <keisha.alexander@hva.nl>
2022-02-28 19:51:43 +0100

    ejs toegevoegd in applicatie

--- server.js
+++ server.js
@@ -1,64 +1,38 @@
 //init app
-const express = require('express');
+const express = require('express'); //module express gebruiken
 
-const app = express();
-const port = 3000;
+const app = express(); //express kopelen aan applicatie
+const port = 3000; //port maken
 const path = require('path');
 
-//load view engine
+//load view engine/template engine
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+app.set('view engine', 'ejs');
 
 
 
 // Template engine
 //The root route
 app.get('/', (req, res) => {
-    let match = [{
-            id: 1,
-            title: 'Dress 1',
-            maker: 'Keïsha Alexander',
-            gepubliceerd: '18 Februari 2022'
 
-
-        },
-
-        {
-            id: 2,
-            title: 'Dress 2',
-            maker: 'Keïsha Alexander',
-            gepubliceerd: '19 Februari 2022'
-
-
-        },
-
-        {
-            id: 3,
-            title: 'Dress 3',
-            maker: 'Keïsha Alexander',
-            gepubliceerd: '20 Februari 2022'
-
-
-        }
-    ];
-
-    res.render('index', {
-        title: 'Hello',
-        matches: match
-    });
+    res.render('index');
 });
 
+
+
+//Formulier pagina om voorkeuren voor jurken te invullen
+app.get('/voorkeuren', (req, res) => {
+    res.render('formulier-voorkeuren')
+});
+
+
+//voorbeeld van dynamische data
 app.get('/match/opslaan', (req, res) => {
     res.render('save-match', {
         title: 'Jouw opgeslagen matches'
     })
 });
 
-
-//Other routes
-app.get('/home', (req, res) => {
-    res.send('Voor de Homepagina')
-});
 
 //Route voor profielpagina's
 app.get('/profiel', (req, res) => {
@@ -71,23 +45,8 @@ app.get('/match', (req, res) => {
 });
 
 
-//Template engine
-// app.set('views', './views');
 
-// const compiledFunction = pug.compileFile('./views/index.pug');
-
-// console.log(compiledFunction({
-//     name: 'Timothy'
-// }));
-
-// app.get('/index', (req, res) => {
-//     res.render('index', {
-//         title: 'Hey',
-//         message: 'Hello there!'
-//     });
-// });
-
-// Error handling
+//een error komt als de route niet bestaat
 app.use((req, res, next) => {
     res.status(404).send('Error 404: File not found')
 });
@@ -95,8 +54,8 @@ app.use((req, res, next) => {
 //serve static files
 app.use('/media', express.static('static'))
 
-//Start server
 
+//de server wordt gestaart op port 3000
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 });
