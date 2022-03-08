16401597c1add657e617bb8e652f9f609d10daf6
K3A101 <keisha.alexander@hva.nl>
2022-03-08 21:13:14 +0100

    Mongodb Database verbonden in app

--- server.js
+++ server.js
@@ -2,101 +2,20 @@
 const express = require("express"); //module express gebruiken
 const bodyParser = require("body-parser");
 const dotenv = require("dotenv").config();
+
+// Mongodb database in app verbinden.
+const {MongoClient }= require("mongodb");
+const {ObjectId }= require("mongodb");
+
+//variabels
 const app = express(); //express kopelen aan applicatie
 const port = 3000; //port maken
 const path = require("path");
-const matches = [{
-        "id": 1,
-        "slug": "dress-1",
-        "url": "dress-1.jpg",
-        "kleur": "rood",
-        "typeJurk": "zomerjurk",
-        "gebruikersnaam": "Annabelle",
-        "publicatiedatum": "12-07-2021",
-        "beschrijving": "T'Challa, heir to the hidden but advanced kingdom of Wakanda, must step forward to lead his people into a new future and must confront a challenger from his country's past."
-    },
-    {
-        "id": 2,
-        "slug": "dress-2",
-        "url": "dress-2.png",
-        "kleur": "geel",
-        "typeJurk": "zomerjurk",
-        "gebruikersnaam": "Minna",
-        "publicatiedatum": "10-12-2021",
-        "beschrijving": "While the Parr family has accepted its collective calling as superheroes, the fact remains that their special heroism is still illegal. After they are arrested after unsuccessfully trying to stop the Underminer, their future seems bleak. However, the wealthy Deavor siblings of Devtech offer new hope with a bold project to rehabilitate the public image and legal status of Supers, with Elastigirl being assigned on point to be the shining example. Now having agreed for now to stay at home to care of the kids, Mr. Incredible finds domestic life a daunting challenge, especially with baby Jack-Jack's newly emerged powers making him almost impossible to manage. However, Elastigirl soon has her own concerns dealing with the menace of a new supervillain, Screenslaver, who is wreaking havoc with his mind control abilities. Now, Elastigirl must solve the mystery of this enemy, who has malevolent designs on the world with the Parr family and friends key targets of this evil. Written by Kenneth Chisholm (kchishol@rogers.com)"
-    },
-    {
-        "id": 3,
-        "slug": "dress-3",
-        "url": "dress-1.jpg",
-        "kleur": "zwart",
-        "typeJurk": "zomerjurk",
-        "gebruikersnaam": "Kea",
-        "publicatiedatum": "19-01-2022",
-        "beschrijving": "Laurie Strode comes to her final confrontation with Michael Myers, the masked figure who has haunted her since she narrowly escaped his killing spree on Halloween night four decades ago."
-    },
-    {
-        "id": 4,
-        "slug": "dress-4",
-        "url": "dress-2.png",
-        "kleur": "donkergroen",
-        "typeJurk": "zomerjurk",
-        "gebruikersnaam": "Millow",
-        "publicatiedatum": "10-10-2021",
-        "beschrijving": "Thirty years ago, Clifford McBride led a voyage into deep space, but the ship and crew were never heard from again. Now his son -- a fearless astronaut -- must embark on a daring mission to Neptune to uncover the truth about his missing father and a mysterious power surge that threatens the stability of the universe."
-    },
-    {
-        "id": 5,
-        "slug": "dress-5",
-        "url": "dress-1.jpg",
-        "kleur": "wit",
-        "typeJurk": "zomerjurk",
-        "gebruikersnaam": "Lisa",
-        "publicatiedatum": "12-05-2021",
-        "beschrijving": "When a new toy called Forky joins Woody and the gang, a road trip alongside old and new friends reveals how big the world can be for a toy."
-    },
-    {
-        "id": 6,
-        "slug": "dress-6",
-        "url": "dress-2.png",
-        "kleur": "paars",
-        "typeJurk": "zomerjurk",
-        "gebruikersnaam": "Belle",
-        "publicatiedatum": "22-10-2021",
-        "beschrijving": "T'Challa, heir to the hidden but advanced kingdom of Wakanda, must step forward to lead his people into a new future and must confront a challenger from his country's past."
-    }
-];
+let db = null;
 
-// const {
-//     MongoClient
-// } = require("mongodb");
-
-// const {
-//     ObjectId
-// } = require("mongodb");
-
-
-// MongoDb server
 
 console.log(process.env.TESTVAR);
 
-
-/*****************************************************
- * Connect to database
- ****************************************************/
-// async function connectDB() {
-//     const uri = process.env.DATABASE_URL;
-//     const client = new MongoClient(uri, {
-//         useNewUrlParser: true,
-//         useUnifiedTopology: true,
-//     });
-//     try {
-//         await client.connect();
-//         db = client.db(process.env.DB_NAME);
-//     } catch (error) {
-//         throw error;
-//     }
-// }
 
 
 //Middleware & static files
@@ -114,7 +33,7 @@ app.set("view engine", "ejs");
 
 
 
-// Template engine
+// TEMPLATE ENGINE
 //The root route
 //Formulier pagina om voorkeuren voor jurken te invullen
 app.get("/", (req, res) => {
@@ -137,6 +56,7 @@ app.post("/voorkeuren", (req, res) => {
 
 app.post("/voorkeuren/:matchId", (req, res) => {
     console.log(req.body);
+
     // FIND MATCHES
     const id = "req.body.id"
     console.log("Get matches from DB")
@@ -172,9 +92,6 @@ app.get("/match", (req, res) => {
     });
 });
 
-
-
-
 //Route voor profielpagina's
 app.get("/profiel", (req, res) => {
     res.render("profiel-pagina", {
@@ -182,16 +99,31 @@ app.get("/profiel", (req, res) => {
     });
 });
 
-
-
-
 //een error 404 komt als de route niet bestaat
 app.use((req, res, next) => {
     res.status(404).render("404");
 });
 
+
+/*****************************************************
+ * Connect to database
+ ****************************************************/
+async function connectDB() {
+    const uri = process.env.DATABASE_URL;
+    const client = new MongoClient(uri, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    });
+    try {
+        await client.connect();
+        db = client.db(process.env.DB_NAME);
+    } catch (error) {
+        throw error;
+    }
+}
+
 //de server wordt gestaart op port 3000
 app.listen(port, () => {
     console.log(`Web server running on http://localhost:${port}`);
-    // connectDB().then(() => console.log("We have a connection to mongo"))
+     connectDB().then(() => console.log("We have a connection to mongo"))
 });
