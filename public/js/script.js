037bc07a26ff61cad804bcb73f6848af83d252a1
K3A101 <keisha.alexander@hva.nl>
2022-03-10 14:43:45 +0100

    Fullscreen Web Api toe te voegen

--- public/js/script.js
+++ public/js/script.js
@@ -4,6 +4,9 @@ console.log('Het werkt!');
 
 const navigatieMenu = document.querySelector("header nav ul");
 const hamburgerMenu = document.querySelector(".menu");
+const images = document.querySelector(".images");
+
+console.log(images);
 
 console.log(navigatieMenu, hamburgerMenu);
 
@@ -13,3 +16,16 @@ function zichtbaarMaken() {
 
 
 hamburgerMenu.addEventListener("click", zichtbaarMaken);
+
+// Fullscreen API
+function activateFullscreen(images) {
+    if (images.requestFullscreen) {
+        images.requestFullscreen(); // W3C spec
+    } else if (images.mozRequestFullScreen) {
+        images.mozRequestFullScreen(); // Firefox
+    } else if (images.webkitRequestFullscreen) {
+        images.webkitRequestFullscreen(); // Safari
+    } else if (images.msRequestFullscreen) {
+        images.msRequestFullscreen(); // IE/Edge
+    }
+};
