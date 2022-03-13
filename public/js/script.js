a94f813861999492b1af632a2da48216dad45114
K3A101 <keisha.alexander@hva.nl>
2022-03-13 20:48:42 +0100

    Eyedropper Api toegevoegd

--- public/js/script.js
+++ public/js/script.js
@@ -1,13 +1,11 @@
 console.log('Het werkt!');
 
-// als je op de hamburger menu klikt wordt de navigatie menu zichtbaar
 
 const navigatieMenu = document.querySelector("header nav ul");
 const hamburgerMenu = document.querySelector(".menu");
 const images = document.querySelector(".images");
 
 console.log(images);
-
 console.log(navigatieMenu, hamburgerMenu);
 
 function zichtbaarMaken() {
@@ -16,6 +14,32 @@ function zichtbaarMaken() {
 
 
 hamburgerMenu.addEventListener("click", zichtbaarMaken);
+
+
+
+
+//maakt eyedropper object
+
+const button = document.querySelector('#eyedropperButton');
+
+if ('EyeDropper' in window) {
+    const eyeDropper = new EyeDropper();
+
+    button.addEventListener('click', () => {
+        eyeDropper
+            .open()
+            .then(colorSelectionResult => {
+                document.body.style.backgroundColor = colorSelectionResult.sRGBHex;
+            })
+            .catch(() => {
+                // The user canceled selection
+            });
+    });
+} else {
+    // The EyeDropper API isn't supported
+}
+// als je op de hamburger menu klikt wordt de navigatie menu zichtbaar
+
 
 // Fullscreen API
 function activateFullscreen(images) {
@@ -29,3 +53,6 @@ function activateFullscreen(images) {
         images.msRequestFullscreen(); // IE/Edge
     }
 };
+
+// Eyedropper Api
+// Bron: https://akhromieiev.com/how-to-use-eyedropper-api/
