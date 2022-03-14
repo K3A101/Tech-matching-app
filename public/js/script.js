1b6812b1203a26989d0f703eade094f288e973a6
K3A101 <keisha.alexander@hva.nl>
2022-03-14 19:50:48 +0100

    Js opmaken

--- public/js/script.js
+++ public/js/script.js
@@ -1,6 +1,6 @@
 console.log('Het werkt!');
 
-
+// als je op de hamburger menu klikt wordt de navigatie menu zichtbaar
 const navigatieMenu = document.querySelector("header nav ul");
 const hamburgerMenu = document.querySelector(".menu");
 const images = document.querySelector(".images");
@@ -12,16 +12,11 @@ function zichtbaarMaken() {
     navigatieMenu.classList.toggle("toon-menu");
 }
 
-
 hamburgerMenu.addEventListener("click", zichtbaarMaken);
-
-
-
 
 //maakt eyedropper object
 
 const button = document.querySelector('#eyedropperButton');
-
 if ('EyeDropper' in window) {
     const eyeDropper = new EyeDropper();
 
@@ -38,21 +33,6 @@ if ('EyeDropper' in window) {
 } else {
     // The EyeDropper API isn't supported
 }
-// als je op de hamburger menu klikt wordt de navigatie menu zichtbaar
-
-
-// Fullscreen API
-function activateFullscreen(images) {
-    if (images.requestFullscreen) {
-        images.requestFullscreen(); // W3C spec
-    } else if (images.mozRequestFullScreen) {
-        images.mozRequestFullScreen(); // Firefox
-    } else if (images.webkitRequestFullscreen) {
-        images.webkitRequestFullscreen(); // Safari
-    } else if (images.msRequestFullscreen) {
-        images.msRequestFullscreen(); // IE/Edge
-    }
-};
 
 // Eyedropper Api
 // Bron: https://akhromieiev.com/how-to-use-eyedropper-api/
