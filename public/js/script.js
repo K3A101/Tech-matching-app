4238a8cd630e9058715c3ce4d00f4650b27d902a
K3A101 <keisha.alexander@hva.nl>
2022-03-18 13:55:08 +0100

    Comments toegevoegd

--- public/js/script.js
+++ public/js/script.js
@@ -1,6 +1,7 @@
 console.log('Het werkt!');
 
 // als je op de hamburger menu klikt wordt de navigatie menu zichtbaar
+//Dom manipulatie
 const navigatieMenu = document.querySelector("header nav ul");
 const hamburgerMenu = document.querySelector(".menu");
 const images = document.querySelector(".images");
@@ -8,10 +9,11 @@ const images = document.querySelector(".images");
 console.log(images);
 console.log(navigatieMenu, hamburgerMenu);
 
+
 function zichtbaarMaken() {
     navigatieMenu.classList.toggle("toon-menu");
 }
-
+// Een click event om de functie te laten uitvoeren.
 hamburgerMenu.addEventListener("click", zichtbaarMaken);
 
 
