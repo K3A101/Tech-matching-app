703814742358fb5602d057322cdf92af5bd77664
K3A101 <keisha.alexander@hva.nl>
2022-03-26 20:28:14 +0100

    Filter functie gefixed

--- server.js
+++ server.js
@@ -68,10 +68,11 @@ app.get("/", async (req, res) => {
     });
 });
 
-// Route voor de form Action: "/homepagina"
-// Dit is de als je method post gebruikt
-app.post("/match/filter", async (req, res) => {
-    console.log(req.body);
+// Filtreren op basis van kleur
+app.post("/match/kleur", async (req, res) => {
+
+
+    console.log(req.body.kleur);
     console.log('hieronder is de object keys loop')
     Object.keys(req.body).forEach(key => {
         console.log(key)
@@ -80,29 +81,24 @@ app.post("/match/filter", async (req, res) => {
     //Krijg gefiltered matches 
     console.log('hieronder matches')
 
-    const dresses = {
-        "id": req.body.id,
-        "slug": req.body.slug,
-        "url": req.body.url,
-        "kleur": req.body.kleur,
-        "typeJurk": req.body.typeJurk,
-        "gebruikersnaam": req.body.gebruikersnaam,
-        "publicatiedatum": req.body.publicatiedatum,
-        "beschrijving": req.body.beschrijving
+    const query = {
+
+        "kleur": req.body.kleur
+
+
     }
-    const matches = await db.collection("jurken").find(dresses).toArray();
+    const matches = await db.collection("jurken").find(query).toArray();
     const noMatches = (matches.length === 0) ? "Helaas is er geen matches gevonden." : "matches";
     console.log(matches)
 
     // Render engine
-     matches.push(match);
+    matches.push(match);
     res.render("filter-resultaten", {
         titel: "Jouw voorkeuren",
         matches,
-        dresses,
         noMatches
     });
-   
+
 });
 
 //Formulier pagina om matches toe te voegen 
@@ -154,7 +150,7 @@ app.post("/homepagina", async (req, res) => {
         beschrijving: req.body.beschrijving
     }
 
-const matches = await db.collection("jurken").insertOne(dresses);
+    const matches = await db.collection("jurken").insertOne(dresses);
     const noMatches = (matches.length === 0) ? "Helaas is er geen matches gevonden." : "matches";
     console.log(matches)
 
